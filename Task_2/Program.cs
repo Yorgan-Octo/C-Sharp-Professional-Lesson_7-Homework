a9f496c557dbe9e1d36358e52a77ead1b2b3d2a9
Artem Priadchenko <yorgan.info@gmail.com>
2023-06-28 00:24:09 +0300

    ЗАвершенно задание 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_2
+{
+
+    public class User
+    {
+        public string Name { get; set; }
+        public string FullName { get; set; }
+        public int Age { get; set; }
+
+
+        public User(string name, string fullName, int age)
+        {
+            Name = name;
+            FullName = fullName;
+            Age = age;
+        }
+
+
+        [Obsolete("Застарілло не потрібен", false)]
+        public void PrintName()
+        {
+            Console.WriteLine($"Имя: {Name} - Фамилия: {FullName};");
+        }
+
+        [Obsolete("Супер застарыло не використовувати", true)]
+        public void PrintAge()
+        {
+            Console.WriteLine($"Вік: {Age};");
+        }
+
+    }
+
+
+
+    internal class Program
+    {
+        static void Main()
+        {
+
+            User user = new User("Ron", "Grif", 23);
+
+            user.PrintName();
+            user.PrintAge();
+
+            Console.WriteLine();
+
+        }
+    }
+}
